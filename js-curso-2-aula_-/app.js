e636dc3b88b46bcd7b5e8c6ecd6c5e29b8d1ed20
Dionatas de Jesus <dionatasdejesus@hotmail.com>
2024-11-16 09:59:48 -0300

    Add files via upload

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,17 @@
+// let titulo = document.querySelector("h1");
+// titulo.innerHTML = "Jodo do Número Secreto";
+
+// let paragrafo = document.querySelector("p");
+// paragrafo.innerHTML = "Escolha um Número de 1 a 100";
+
+function verificarChute() {
+  console.log("O botão esta ativo");
+}
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector("tag");
+  campo.innerHTML = "texto";
+}
+
+exibirTextoNaTela("h1", "Jodo do Número Secreto");
+exibirTextoNaTela("p", "Escolha um Número de 1 a 100");
